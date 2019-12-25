4a5557926e9da36444216b388649847bb9e119f3
edenohayon <eden.moyal1994@gmail.com>
2019-12-25 11:41:14 +0200

    splash screen

---
+++ app/src/main/java/com/example/androidfinalproject/SplashScreenActivity.java
@@ -0,0 +1,41 @@
+package com.example.androidfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        EasySplashScreen config = new EasySplashScreen(SplashScreenActivity.this)
+                .withFullScreen()
+                .withTargetActivity(MainActivity.class)
+                .withSplashTimeOut(2000)
+                .withBackgroundResource(android.R.color.darker_gray)
+                .withHeaderText("Header")
+                .withFooterText("Copyright 2016")
+                .withBeforeLogoText("My cool company")
+                .withAfterLogoText("Some more details")
+                .withLogo(R.mipmap.ic_launcher_round);
+
+        config.getHeaderTextView().setTextColor(Color.WHITE);
+        config.getFooterTextView().setTextColor(Color.WHITE);
+        config.getBeforeLogoTextView().setTextColor(Color.WHITE);
+        config.getAfterLogoTextView().setTextColor(Color.WHITE);
+
+        View easySplashScreen = config.create();
+        setContentView(easySplashScreen);
+
+//        setContentView(easySplashScreenView);
+
+    }
+}
+//                .withLogo(R.drawable.logo)
