f3141eb40e06b5b955dcace0a70b40aee0452f04
edenohayon <eden.moyal1994@gmail.com>
2019-12-27 18:22:52 +0200

    main activity add botton and popup to get item name. and some more design

--- app/src/main/java/com/example/androidfinalproject/MainActivity.java
+++ app/src/main/java/com/example/androidfinalproject/MainActivity.java
@@ -2,18 +2,25 @@ package com.example.androidfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Icon;
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import static android.view.Gravity.CENTER;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +30,11 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Button> items;
     private TableRow.LayoutParams lp;
 
-    private int numInRow;
+    private int itemsAdded;
+
+    // for popup
+    Dialog myDialog;
+    boolean isPopupWarningOn;
 
     private final int NUM_COL = 3;
     @Override
@@ -31,59 +42,111 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //currently holding only added items
         items = new ArrayList<Button>();
         tableLayout = findViewById(R.id.tableLayout);
-        tableRow = findViewById(R.id.row0);
-        lp = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT, 1.0f);
-        numInRow = 0;
+       // tableRow = findViewById(R.id.row0);
+       // lp = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT, 1.0f);
+        itemsAdded = 0;
 
         add = findViewById(R.id.addBtn);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                 addNewItem();
+                 showPopup();
             }
         });
 
-        //this is my edit
+        myDialog = new Dialog(this);
+        isPopupWarningOn = false;
 
     }
 
-    private void addNewItem() {
+    private void showPopup() {
+        TextView txtclose;
+        Button btnFollow;
+        myDialog.setContentView(R.layout.popup);
+        txtclose =(TextView) myDialog.findViewById(R.id.txtclose);
+        txtclose.setText("M");
+        btnFollow = (Button) myDialog.findViewById(R.id.btnfollow);
 
-        Button tmp = new Button(this);
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
 
-        //image
-        tmp.setBackgroundResource(R.drawable.download);
+        btnFollow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        //set image size and style
-        tmp.setLayoutParams(new TableRow.LayoutParams(
-                TableRow.LayoutParams.WRAP_CONTENT,
-                TableRow.LayoutParams.WRAP_CONTENT,
-                1.0f));
+                // Inflate using dialog themed context. - mabe not neseccery
+                final Context context = myDialog.getContext();
+                final LayoutInflater inflater = LayoutInflater.from(context);
+                final View view = inflater.inflate(R.layout.popup, null, false);
 
-        lp.height=150;
-        lp.width = 150;
-        tmp.setPadding(100,110,1,1);
+                final  EditText input = myDialog.findViewById(R.id.input);
+                String inputVal = input.getText().toString();
 
-        tmp.setLayoutParams(lp);
-        items.add(tmp);
+                if(inputVal.isEmpty())
+                {
 
-        //max items per row
-        if(numInRow == NUM_COL)
+                    if(!isPopupWarningOn) {
+                        Log.d("debug", "onClick: input emty");
+                        TextView warning = new TextView(myDialog.getContext());
+                        warning.setTextColor(Color.RED);
+                        warning.setText("הכנס שם מוצר");
+                        warning.setGravity(CENTER);
+                        warning.setLayoutParams(new LinearLayout.LayoutParams(
+                                TableLayout.LayoutParams.MATCH_PARENT,
+                                TableLayout.LayoutParams.MATCH_PARENT));
+                        LinearLayout layout = myDialog.findViewById(R.id.linearLayout);
+                        layout.addView(warning);
+                    }
+                    isPopupWarningOn = true;
+
+                    return;
+                }
+
+                addNewItem(inputVal);
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
+
+
+
+    }
+
+    private void addNewItem(String inputVal) {
+
+        Button button = new Button(this);
+
+        items.add(button);
+
+        // need to create new row
+        if(itemsAdded == 0)
         {
             TableRow tableRowTmp = new TableRow(this);
-            tableRow.setLayoutParams(new TableLayout.LayoutParams(
+            tableRowTmp.setLayoutParams(new TableLayout.LayoutParams(
                     TableLayout.LayoutParams.MATCH_PARENT,
-                    TableLayout.LayoutParams.MATCH_PARENT,
-                    1.0f));
+                    TableLayout.LayoutParams.MATCH_PARENT));
             tableLayout.addView(tableRowTmp);
             tableRow = tableRowTmp;
-            numInRow = 0;
+
         }
 
-        tableRow.addView(tmp);
-        numInRow++;
+        button.setLayoutParams(new TableRow.LayoutParams(
+                TableRow.LayoutParams.WRAP_CONTENT,
+                TableRow.LayoutParams.WRAP_CONTENT));
+        button.setText(inputVal);
+
+        tableRow.addView(button);
+        itemsAdded++;
+        if(itemsAdded == NUM_COL)
+            itemsAdded = 0;
 
     }
 
