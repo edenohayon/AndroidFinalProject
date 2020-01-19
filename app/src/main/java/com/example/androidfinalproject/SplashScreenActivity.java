e1654840bb08f8b613a3e7d5cab68e4341d40b80
edenohayon <eden.moyal1994@gmail.com>
2020-01-19 14:24:26 +0200

    שיפור מחיקה של פריט

--- app/src/main/java/com/example/androidfinalproject/SplashScreenActivity.java
+++ app/src/main/java/com/example/androidfinalproject/SplashScreenActivity.java
@@ -24,64 +24,23 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen);
 
-
-        //EasySplashScreen config;
         Thread thread = new Thread() {
 
             public void run() {
+                try {
+                    sleep(2000);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
 
-
-                        try {
-                            sleep(2000);
-                            finish();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        finally {
-//                            if (!checkConnectivity()) {
-//                                Toast.makeText(getBaseContext(), "שגיאה. אנא וודא כי קיים חיבור לרשת", Toast.LENGTH_LONG).show();
-//                                Log.d("debug -------- ", "splash: ");
-//                                finish();
-//                                finishAffinity();
-//                            } else
-                                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-                        }
-                    }
-                };
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                }
+            }
+        };
 
         thread.start();
 
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-
-    }
-
-
-    private boolean checkConnectivity() {
-        boolean hasWifi = false;
-        boolean hasMobileData = false;
-        ConnectivityManager connectivity = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (connectivity != null) {
-            NetworkInfo info = connectivity.getActiveNetworkInfo();
-            if (info != null) {
-                if (info.getType() == ConnectivityManager.TYPE_WIFI) {
-                    if (info.isConnected())
-                        hasWifi = true;
-                }
-                if (info.getType() == ConnectivityManager.TYPE_MOBILE) {
-                    if (info.isConnected())
-                        hasMobileData = true;
-                }
-            }
-
-
-        }
-
-        return hasMobileData || hasWifi;
     }
 
 
